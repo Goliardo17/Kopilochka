088210a438bde4ea7b35073017a44026d89a1492
Goliardo17 <goliardo17@yandex.ru>
2024-07-30 23:31:17 +0300

    middleware fixed

--- server/middlewares/authMiddleware/index.js
+++ server/middlewares/authMiddleware/index.js
@@ -3,15 +3,16 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   try {
-    const token = req.headers.authorization
+    const token = req.headers.authorization;
 
     if (!token) {
-        throw new Error ("Пользователь не авторизован")
+      res.status(403).json({ error: "Пользователь не авторизован" });
+      return;
     }
 
-    const decoded = jwt.verify(token, secret)
-    req.user = decoded
-    next()
+    const decoded = jwt.verify(token, secret);
+    req.user = decoded;
+    next();
   } catch (err) {
     res.status(400).json(err.message);
   }
